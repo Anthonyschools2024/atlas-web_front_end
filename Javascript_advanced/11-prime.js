d0da7b55be49f829463e4bb70e42d47cc8904849
Frank Grove <fragro@gmail.com>
2025-03-30 22:30:00 -0500

    binding

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,31 @@
+// Helper function to check if a number is prime
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+// Function that returns the number of prime numbers from 2 to 100
+function countPrimeNumbers() {
+  let count = 0;
+  for (let num = 2; num <= 100; num++) {
+    if (isPrime(num)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Use setTimeout to defer the heavy calculation to the end of the execution stack
+setTimeout(function() {
+  const startTime = performance.now();
+  for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+  }
+  const endTime = performance.now();
+  const executionTime = endTime - startTime;
+  
+  console.log(`Execution time of calculating prime numbers 100 times was ${executionTime} milliseconds.`);
+}, 0);
