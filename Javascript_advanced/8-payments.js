eccf33a86ff9328d7441215ea96d4ca634f37b81
Frank Grove <fragro@gmail.com>
2025-03-30 22:13:48 -0500

    stack order in functions

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,18 @@
+// Function to process the payment
+function processPayment(amount) {
+  console.log("Collecting payment of " + amount);
+}
+
+// Function to process the order
+function processOrder(orderId, amount) {
+  console.log(orderId + " is being processed");
+  processPayment(amount);
+  console.log(orderId + " has been fully processed");
+}
+
+// Main code execution
+console.log("Processing orders");
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.0);
+console.log("All the orders have been processed");
